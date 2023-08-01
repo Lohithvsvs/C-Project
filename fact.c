4e56fa57b161c418806195fe0763b302388711ee
Lohith <Lohithvsvs@gmail.com>
2023-08-01 19:19:04 +0530

    modified fact.c

--- fact.c
+++ fact.c
@@ -1,20 +1,12 @@
- #include<stdio.h>    
-void printFibonacci(int n){    
-    static int n1=0,n2=1,n3;    
-    if(n>0){    
-         n3 = n1 + n2;    
-         n1 = n2;    
-         n2 = n3;    
-         printf("%d ",n3);    
-         printFibonacci(n-1);    
-    }    
-}    
-int main(){    
-    int n;    
-    printf("Enter the number of elements: ");    
-    scanf("%d",&n);    
-    printf("Fibonacci Series: ");    
-    printf("%d %d ",0,1);    
-    printFibonacci(n-2);//n-2 because 2 numbers are already printed    
-  return 0;  
- }    
+#include<stdio.h>  
+int main()    
+{    
+ int i,fact=1,number;    
+ printf("Enter a number: ");    
+  scanf("%d",&number);    
+    for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  printf("Factorial of %d is: %d",number,fact);    
+return 0;  
+}   
